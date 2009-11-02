3cac34dfea13911c68460f6aef6aa322db751b44
James Gibson <me@iamjamesgibson.com>
2009-11-02 11:22:28 -0500

    added closure around jQuery select-autocomplete plugin, added in options that can be passed to the original autocomplete, removed extra use of jquery object calls since caching jquery objects is faster, var all variables at the top of methods and only used one var

--- jquery.select-autocomplete.js
+++ jquery.select-autocomplete.js
@@ -1,52 +1,73 @@
-jQuery.fn.select_autocomplete = function() {
-  return this.each(function(){
-		if (this.tagName.toLowerCase() != 'select') { return; }
-	
-    //stick each of it's options in to an items array of objects with name and value attributes 
-    var select = this;
-    var items = [];    
-    $(select).children('option').each(function(){
-      var item = $(this);
-      if (item.val() != '') //ignore empty value options
-      {
-        var name = item.html();
-        var value = item.val();
-        items.push( {'name':name, 'value':value} );
-      }
-    });
+(function($) {
+		  
+	$.fn.select_autocomplete = function(options) {
+		
+		// make sure we have an options object
+		options = options || {};
+		
+		// setup our defaults
+		var defaults = { 
+			  minChars: 0
+			, width: 310
+			, matchContains: true
+			, autoFill: false
+			, formatItem: function(row, i, max) {
+				return row.name;
+			}
+			, formatMatch: function(row, i, max) {
+				return row.name;
+			}
+			, formatResult: function(row) {
+				return row.name;
+			}
+		};
+		
+		options = $.extend(defaults, options);
+		
+		return this.each(function() {
+			
+			if (this.tagName.toLowerCase() != 'select') { return; }
+		
+			//stick each of it's options in to an items array of objects with name and value attributes 
+			var select = this,
+				$this = $(this),
+				data = [],
+				$input = $('<input type="text" />');
+				
+			
+			$this.children('option').each(function() {
+		
+				var $option = $(this);
+				
+				if ($option.val() != '') { //ignore empty value options
 
-    //make a new input box next to the select list
-    var input = $("<input type='text' />").appendTo('body');
-    $(select).after(input);
-
-    //make the input box into an autocomplete for the select items
-    $(input).autocomplete(items, {      
-      data: items,  
-      minChars: 0,
-      width: 310,
-      matchContains: false,
-      autoFill: false,
-      formatItem: function(row, i, max) {
-        return row.name;
-      },
-      formatMatch: function(row, i, max) {
-        return row.name;
-      },
-      formatResult: function(row) {
-        return row.name;
-      }
-    });
-
-    //make the result handler set the selected item in the select list
-    $(input).result(function(event, selected_item, formatted) { 
-      var to_be_selected = $(select).find("option[value=" + selected_item.value + "]")[0];
-      $(to_be_selected).attr('selected', 'selected');
-    });
-
-    //set the initial text value of the autocomplete input box to the text node of the selected item in the select control
-    $(input).val($(select).find(':selected').text());
-
-    //normally, you'd hide the select list but we won't for this demo
-    $(select).hide();
-  });
-};
+					data.push({
+						  name: $option.html()
+						, value:$option.val()
+					});
+				}
+			});
+			
+			// insert the input after the select
+			$this.after($input);
+			
+			// add it our data
+			options.data = data;
+		
+			//make the input box into an autocomplete for the select items
+			$input.autocomplete(data, options);
+		
+			//make the result handler set the selected item in the select list
+			$input.result(function(event, selected_item, formatted) { 
+				$($this.find('option[value=' + selected_item.value + ']')[0]).attr('selected', true);
+			});
+		
+			//set the initial text value of the autocomplete input box to the text node of the selected item in the select control
+			$input.val($(select).find(':selected').text());
+		
+			//normally, you'd hide the select list but we won't for this demo
+			$this.hide();
+		});
+	};		  
+  
+})(jQuery);
