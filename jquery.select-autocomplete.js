9f52ab854136c53588945e48d3994fc376c1db91
James Gibson <me@iamjamesgibson.com>
2009-11-03 14:06:30 -0500

    update to script to remove all references to the dom object and to change around setting the inputs value to only the selected item.

--- jquery.select-autocomplete.js
+++ jquery.select-autocomplete.js
@@ -25,14 +25,13 @@
 		options = $.extend(defaults, options);
 		
 		return this.each(function() {
-			
-			if (this.tagName.toLowerCase() != 'select') { return; }
 		
 			//stick each of it's options in to an items array of objects with name and value attributes 
-			var select = this,
-				$this = $(this),
+			var $this = $(this),
 				data = [],
 				$input = $('<input type="text" />');
+			
+			if (this.tagName.toLowerCase() != 'select') { return; }
 				
 			
 			$this.children('option').each(function() {
@@ -63,7 +62,7 @@
 			});
 		
 			//set the initial text value of the autocomplete input box to the text node of the selected item in the select control
-			$input.val($(select).find(':selected').text());
+			$input.val($this.find('[selected=selected]').text());
 		
 			//normally, you'd hide the select list but we won't for this demo
 			$this.hide();
